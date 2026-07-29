95323a775548a8b3e5be978bde8f711a5f395f38
Vo Hong Phat <vohongphat2892005@gmail.com>
2026-07-29 15:40:26 +0700

    set viper port

--- cmd/cli/viper/main.viper.go
+++ cmd/cli/viper/main.viper.go
@@ -1,2 +1,24 @@
-package viper
+package main
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper := viper.New()
+	viper.AddConfigPath("./config/") //path to config
+
+	viper.SetConfigName("production") //ten file
+	viper.SetConfigType("yaml")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Failed to read config %w \n", err))
+	}
+	//write server config
+	fmt.Println("Server Port::", viper.GetInt("server.port"))
+	fmt.Println("Security Key Port::", viper.GetString("security.jwt.key"))
+
+}
